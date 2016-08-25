895f2f559616025e824346d9a9f00704a8a5388c
Aliou Diallo <aliou@followanalytics.com>
2016-08-25 17:33:51 +0200

    Allow configuration of the gem.

--- spec/followanalytics_spec.rb
+++ spec/followanalytics_spec.rb
@@ -5,7 +5,19 @@ describe Followanalytics do
     expect(Followanalytics::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '.configure' do
+    it 'uses the default API url if none is given' do
+      expect(Followanalytics.configuration.api_base_url).
+        to eq(Followanalytics::Configuration::DEFAULT_API_BASE_URL)
+    end
+
+    it 'uses the given API url if given' do
+      base_url = Faker::Internet.url
+      Followanalytics.configure do |config|
+        config.api_base_url = base_url
+      end
+
+      expect(Followanalytics.configuration.api_base_url).to eq(base_url)
+    end
   end
 end
